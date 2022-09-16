649e636c64a76dd2fe837276cafca4e4d6c4a3ad
Erdal ALTIN <erdalaltin@gmail.com>
2022-09-16 12:15:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ echo "============================================"
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(grep -Po 'UBUNTU_CODENAME=\K[^;]*' /etc/os-release) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo chmod 666 /var/run/docker.sock
 sudo apt-get update
 
@@ -228,6 +228,7 @@ if [ -x "$(command -v docker)" ] && [ -x "$(command -v docker-compose)" ]; then
 	if [ $? -eq 0 ]
 	then
 		# installing portainer
+		docker volume create portainer_data > /dev/null
 		docker-compose -f portainer-docker-compose.yml -p portainer up -d & export pid=$!
 		echo ""
 		echo "portainer installing proceeding..."
@@ -236,6 +237,7 @@ if [ -x "$(command -v docker)" ] && [ -x "$(command -v docker-compose)" ]; then
 			echo "Error! could not installed portainer" >&2
 			exit 1
 		else
+			echo ""
 			until [ -n "$(sudo find ./certbot/live -name '$domain_name' 2>/dev/null | head -1)" ]; do
 				echo "waiting for Let's Encrypt certificates for $domain_name"
 				sleep 5s & wait ${!}
